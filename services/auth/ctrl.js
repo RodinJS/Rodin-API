8d0dc14f5c0b0f1524cb4fae213024426bfa0b70
Grigør <g@yvn.io>
2017-09-20 16:34:10 +0400

    RO-1263 # Dockerize API

--- services/auth/ctrl.js
+++ services/auth/ctrl.js
@@ -206,6 +206,7 @@ function socialAuth(req) {
                 }
                 console.log(`user ------- to object `, user.toObject())
                 console.log(`user ------- no to `, user.toObject())
+                console.log(`if paymanner --------------   ${req.params.socialName} |||||| ${user.github}`);
                 if(user.github && req.params.socialName == 'github') return reject(Response.onError(null, httpStatus.GIT_ALREADY_SYNCED.message, 400));
                 return _updateUserFromSocial(req, user);
             })
